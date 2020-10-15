bea71b2d0e75cd9182e38ee8aa6787862371798b
joser834 <joser834@gmail.com>
2020-10-15 00:19:29 -0600

    adding digit recognition project

--- Digit_Recognition/mnist/part1/linear_regression.py
+++ Digit_Recognition/mnist/part1/linear_regression.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-### Functions for you to fill in ###
-
 def closed_form(X, Y, lambda_factor):
     """
     Computes the closed form solution of linear regression with L2 regularization
@@ -15,10 +13,41 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    A = np.dot(X.T,X)
+    B = lambda_factor*np.eye(X.shape[1])
+    C = np.dot(X.T,Y)
+    theta = np.dot(np.linalg.inv(A+B),C)
+    return(theta)
 
-### Functions which are already complete, for you to use ###
+# Example -----------------------------------------------------------------------
+# x = np.array([
+#     [0.22710231, 0.22990637],
+#     [0.41483016, 0.66602953],
+#     [0.36395381, 0.08855222],
+#     [0.10284558, 0.63802993],
+#     [0.60193562, 0.50336436],
+#     [0.4398084 , 0.86708208],
+#     [0.15649144, 0.16755288],
+#     [0.32097052, 0.37398561],
+#     [0.39521456, 0.67778334],
+#     [0.41899049, 0.8217538 ],
+#     [0.08517112, 0.98952968],
+#     [0.5689853 , 0.51093225],
+#     [0.58355097, 0.61763799],
+#     [0.29104132, 0.09186201],
+#     [0.59548743, 0.32567937],
+#     [0.16733378, 0.64308253],
+#     [0.4626983 , 0.16623097],
+#     [0.20936176, 0.57699717],
+#     [0.75323588, 0.42076979]
+# ])
+
+# y = [0.6180502, 0.11414268, 0.16354983, 0.45525746, 0.12089474, 0.78347523, 0.90503838 ,0.72594625, 0.22589511, 0.08951521, 0.69720362, 0.60441882,
+# 0.68373848 ,0.86066428, 0.9487789 , 0.99579397, 0.45937451, 0.73058468, 0.71347492]
+
+# lambda_f = 0.23314019247300644
+
+# print(closed_form(x, y, lambda_f))
 
 def compute_test_error_linear(test_x, Y, theta):
     test_y_predict = np.round(np.dot(test_x, theta))
--- Digit_Recognition/mnist/part1/main.py
+++ Digit_Recognition/mnist/part1/main.py
@@ -16,7 +16,7 @@ from kernel import *
 # Load MNIST data:
 train_x, train_y, test_x, test_y = get_MNIST_data()
 # Plot the first 20 images of the training set.
-plot_images(train_x[0:20, :])
+# plot_images(train_x[0:20, :])
 
 #######################################################################
 # 2. Linear Regression with Closed Form Solution
@@ -41,7 +41,7 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=1))
+print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
 
 
 #######################################################################
